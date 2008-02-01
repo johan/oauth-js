4f3fa6c9753199a9e0e64344552493a00280ef96
John Kristian <jkristian@netflix.com>
2008-02-01 01:26:03 +0000

    echo data from term.ie or mediamatic.nl
    git-svn-id: http://oauth.googlecode.com/svn/code/javascript@447 f7ae4463-c52f-0410-a7dc-93bad6e629e8

--- v2/example/consumer.js
+++ v2/example/consumer.js
@@ -2,10 +2,11 @@ var termie =
 { consumerKey   : "key"
 , consumerSecret: "secret"
 , serviceProvider:
-  { requestTokenURL     : "http://term.ie/oauth/example/request_token.php"
+  { signatureMethod     : "HMAC-SHA1"
+  , requestTokenURL     : "http://term.ie/oauth/example/request_token.php"
   , userAuthorizationURL: "accessToken.html" // a stub
   , accessTokenURL      : "http://term.ie/oauth/example/access_token.php"
-  , signatureMethod     : "HMAC-SHA1"
+  , echoURL             : "http://term.ie/oauth/example/echo_api.php"
   }
 };
 
@@ -13,14 +14,15 @@ var mediamatic =
 { consumerKey   : "e388e4f4d6f4cc10ff6dc0fd1637da370478e49e2"
 , consumerSecret: "0b062293b6e29ec91a23b2002abf88e9"
 , serviceProvider:
-  { requestTokenURL     : "http://oauth-sandbox.mediamatic.nl/module/OAuth/request_token"
+  { signatureMethod     : "HMAC-SHA1"
+  , requestTokenURL     : "http://oauth-sandbox.mediamatic.nl/module/OAuth/request_token"
   , userAuthorizationURL: "http://oauth-sandbox.mediamatic.nl/module/OAuth/authorize"
   , accessTokenURL      : "http://oauth-sandbox.mediamatic.nl/module/OAuth/access_token"
-  , signatureMethod     : "HMAC-SHA1"
+  , echoURL             : "http://oauth-sandbox.mediamatic.nl/services/rest/?method=anymeta.test.echo"
   }
 };
 
-var consumer = termie;
+var consumer = mediamatic;
 
 consumer.signForm =
 function signForm(form, etc) {
@@ -29,17 +31,21 @@ function signForm(form, etc) {
                    , tokenSecret   : etc.tokenSecret.value};
     var message = { action: form.action
                   , method: form.method
-                  , parameters: { oauth_consumer_key    : form.oauth_consumer_key.value
-                                , oauth_signature_method: form.oauth_signature_method.value
-                                }
+                  , parameters: {}
                   };
-    if (form.oauth_token != null) {
-        OAuth.setParameter(message, "oauth_token", form.oauth_token.value);
+    for (var e = 0; e < form.elements.length; ++e) {
+        var input = form.elements[e];
+        if (input.name != null && input.name != "" && input.value != null) {
+            message.parameters[input.name] = input.value;
+        }
     }
     OAuth.setTimestampAndNonce(message);
     OAuth.SignatureMethod.sign(message, accessor);
-    for (var p in OAuth.getParameterMap(message.parameters)) {
-        form[p].value = message.parameters[p];
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+    for (var p in parameterMap) {
+        if (form[p] != null && form[p].name != null && form[p].name != "") {
+            form[p].value = parameterMap[p];
+        }
     }
     return true;
 };
